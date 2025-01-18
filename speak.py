966966fbcd416ab2df1a992cfcb8b4ebb76dbfe6
Ray Chen <rc144@outlook.com>
2025-01-18 16:13:09 -0500

    code for testing

--- speak.py
+++ speak.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 from collections import deque
 from threading import Thread
 
@@ -7,13 +8,16 @@ import pyttsx3
 
 class SpeechClient:
     engine: pyttsx3.Engine
-    _speech_queue: deque[str] = deque()
+    _speech_queue: deque[str]
     _speech_task = None
     _current_speaking_thread = None
 
-    def __init__(self):
+    def __init__(self, speech_queue: deque[str]):
         self.engine = pyttsx3.init()
         self.engine.setProperty('rate', 130)
+        thread = Thread(target=self._process_queue)
+        self._speech_queue = speech_queue
+        thread.start()
 
     def queue_text(self, text: str):
         self._speech_queue.append(text)
@@ -21,35 +25,20 @@ class SpeechClient:
     def _speak(self, text: str) -> None:
         self.engine.say(text)
         self._current_speaking_thread = Thread(target=self.engine.runAndWait)
-        self._current_speaking_thread.run()
+        return self._current_speaking_thread.start()
 
-    async def start_speaking(self):
-        if not self._speech_task:
-            self._speech_task = asyncio.create_task(self._process_queue())
-            await self._speech_task
-
-
-    async def stop_speaking(self):
-        if self._speech_task:
-            self._speech_task.cancel()
-        self._speech_task = None
-
-    async def _process_queue(self):
+    def _process_queue(self):
         while True:
+            print(self.engine.isBusy())
             if self._speech_queue and not (self._current_speaking_thread and self._current_speaking_thread.is_alive()):
                 self._speak(self._speech_queue.popleft())
-            await asyncio.sleep(0)
+            time.sleep(0.5)
 
 async def main():
-    my_engine = SpeechClient()
+    my_engine = SpeechClient(deque())
     my_engine.queue_text('fuck this shit im out')
     my_engine.queue_text('thats whats up')
     my_engine.queue_text('the only way is through')
-    try:
-        await my_engine.start_speaking()
-    except KeyboardInterrupt:
-        await my_engine.stop_speaking()
-        return
 
 
 if __name__ == '__main__':
