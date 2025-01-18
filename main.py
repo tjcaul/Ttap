fd56296ed1c2c22c141784649b2429e1b299c462
sark <sark.asadourian@icloud.com>
2025-01-18 16:10:26 -0500

    main is done

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-import time
-from PIL import ImageGrab, Image
-import ocr
-import speak
+from collections import deque
+from ocr import OCR
+from speak import SpeechClient
+from gui import App
 
 def mainloop(delay: float):
-    time.sleep(2)
-    speak_engine = speak.init()
+    POLL_RATE = 0.1
+
+    global_que = deque()
+    speak_engine = SpeechClient(global_que)
+    ocr_engine = OCR(global_que, POLL_RATE)
+    gui = App(ocr_engine, speak_engine)
+
+
 
     if __name__ == '__main__':
         mainloop(0.1)
