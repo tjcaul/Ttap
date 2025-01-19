1b12cd8abf41620a0ba55dd2555c688661446a50
Tyson Caul <tjcaul@icloud.com>
2025-01-19 04:19:39 -0500

    Clean up requirements

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from ocr import OCR
 from speak import SpeechClient
 from gui import App
 
-def mainloop():
-    POLL_RATE = 0.1
+def main():
+    POLL_RATE = 0.025
 
     global_que = deque()
     speak_engine = SpeechClient(global_que, profile_time=True)
@@ -12,8 +12,5 @@ def mainloop():
     ui = App(ocr_engine, speak_engine)
     ui.mainloop()
 
-
-
-
 if __name__ == '__main__':
-    mainloop()
+    main()
